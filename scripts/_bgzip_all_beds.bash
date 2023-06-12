2e41811dc0d93117f3f8c1e3a6037d227de10b0e
kopardev <vishal.koparde@nih.gov>
2023-06-12 10:57:26 -0400

    updates to _bgzip_all_beds script

--- scripts/_bgzip_all_beds.bash
+++ scripts/_bgzip_all_beds.bash
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 FOLDER=$1
+sleep 10
 cd $FOLDER
+nbeds=$(ls *.bed 2>/dev/null | wc -l)
+if [ "$nbeds" != "0" ];then
 for f in `ls *.bed`;do
-	bedSort $f $f
-	bgzip -f --threads 2 $f
-	tabix -f -p bed ${f}.gz
+        bedSort $f $f
+        bgzip -f --threads 2 $f
+        tabix -f -p bed ${f}.gz
 done
+fi
